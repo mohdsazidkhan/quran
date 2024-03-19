4aab8da5eca977c60a3b7b06f09e5917afe21116
SazidKhan <khanmohdsazid@gmail.com>
2024-03-19 23:43:45 +0530

    First commit

---
+++ build/precache-manifest.0a4f8d6969c092dfcf2285df4d2d06b2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "64745284a57ae6cefc6a",
+    "url": "./static/css/main.aaed337f.chunk.css"
+  },
+  {
+    "revision": "64745284a57ae6cefc6a",
+    "url": "./static/js/main.64745284.chunk.js"
+  },
+  {
+    "revision": "23451b01974c8b9629c4",
+    "url": "./static/css/1.cf6a755a.chunk.css"
+  },
+  {
+    "revision": "23451b01974c8b9629c4",
+    "url": "./static/js/1.23451b01.chunk.js"
+  },
+  {
+    "revision": "4a686d48d5a089750c49",
+    "url": "./static/js/runtime~main.4a686d48.js"
+  },
+  {
+    "revision": "7160cd22213c17c638ac78ccf56795d4",
+    "url": "./index.html"
+  }
+];
